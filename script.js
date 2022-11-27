ede5ed2074b3fa340cc4bd17ac6ceb63d7f1c2ec
nabilfikrisp <mnabilfikrisp@gmail.com>
2022-11-27 11:09:16 +0700

    benerin vigenere cipher

--- script.js
+++ script.js
@@ -37,6 +37,73 @@ function encode() {
   }
 
   if (key.length > text.length) {
+    key = key.slice(0, text.length + 1);
+  }
+
+  text = text.split('');
+  let charIndex = 0;
+  let charValue = '';
+  let symbolDict = getAllIndexes(text,);
+
+  let upperText = [];
+  for (let i = 0; i < text.length; i++) {
+    charIndex = i;
+    charValue = text[i];
+    if (!/^[a-zA-Z]*$/.test(text[i])) {
+      // console.log(text.indexOf(char, charIndex + 1));
+      symbolDict.push({
+        value: charValue,
+        index: charIndex
+      })
+    }
+    if (/^[A-Z]*$/.test(text[i])) {
+      upperText.push(charIndex)
+    }
+  };
+  console.log(symbolDict)
+  console.log(upperText)
+
+  text = text.join('').replace(/[^a-zA-Z]+/g, '').toLowerCase();
+
+  key = generateKey(text, key).toLowerCase();
+
+
+  console.log(text);
+  console.log(key);
+
+  let cipherText = '';
+  let calculated = 0;
+  for (let i = 0; i < text.length; i++) {
+
+    // console.log(text.charCodeAt(i));
+    // console.log(key.charCodeAt(i));
+    calculated = (text.charCodeAt(i) + key.charCodeAt(i) - 194) % 26;
+    cipherText += String.fromCharCode(calculated + 97);
+  }
+
+  for (let i in symbolDict) {
+    cipherText = cipherText.slice(0, symbolDict[i]['index']) + symbolDict[i]['value'] + cipherText.slice(symbolDict[i]['index'])
+  }
+
+  cipherText = cipherText.split('');
+  for (let i in upperText) {
+    cipherText[upperText[i]] = cipherText[upperText[i]].toUpperCase();
+  }
+  cipherText = cipherText.join('');
+  result.innerHTML = cipherText;
+}
+
+function decode() {
+  let text = document.querySelector('.text').value;
+  let key = document.querySelector('.key').value;
+  let result = document.querySelector('.result');
+
+  if (!text || !key) {
+    alert('text atau key tidak boleh kosong!');
+    return;
+  }
+
+  if (key.length > text.length) {
     alert('key tidak bisa lebih panjang dari text');
     return;
   }
@@ -68,7 +135,7 @@ function encode() {
 
   key = generateKey(text, key).toLowerCase();
 
-  
+
   console.log(text);
   console.log(key);
 
@@ -78,41 +145,56 @@ function encode() {
 
     // console.log(text.charCodeAt(i));
     // console.log(key.charCodeAt(i));
-    calculated = (text.charCodeAt(i) + key.charCodeAt(i) - 194) % 26;
+    calculated = (text.charCodeAt(i) - key.charCodeAt(i));
+    if(calculated < 0){
+      
+      calculated = 26 + calculated;
+      console.log(text.charCodeAt(i));
+      console.log(key.charCodeAt(i));
+      console.log(calculated);
+    }
+    calculated = calculated ;
+
+
     cipherText += String.fromCharCode(calculated + 97);
   }
 
-  for(let i in symbolDict){
+  for (let i in symbolDict) {
     cipherText = cipherText.slice(0, symbolDict[i]['index']) + symbolDict[i]['value'] + cipherText.slice(symbolDict[i]['index'])
   }
 
   cipherText = cipherText.split('');
-  for(let i in upperText){
-    cipherText[upperText[i]] = cipherText[upperText[i ]].toUpperCase();
+  for (let i in upperText) {
+    cipherText[upperText[i]] = cipherText[upperText[i]].toUpperCase();
   }
   cipherText = cipherText.join('');
   result.innerHTML = cipherText;
 }
 
-function decode() {
-  const text = document.querySelector('.text').value;
-  const key = document.querySelector('.key').value;
-  console.log(text);
-  console.log(key);
+function process(){
+  let encodeBtn = document.querySelector('#showEncode')
+  let decodeBtn = document.querySelector('#showDecode')
+
+  if(encodeBtn.checked){
+    encode()
+  }
+  if(decodeBtn.checked){
+    decode()
+  }
 }
 
-function showEncode() {
-  const encodeDiv = document.querySelector('.encodeDiv');
-  const decodeDiv = document.querySelector('.decodeDiv');
+// function showEncode() {
+//   const encodeDiv = document.querySelector('.encodeDiv');
+//   const decodeDiv = document.querySelector('.decodeDiv');
 
-  encodeDiv.style.display = "block";
-  decodeDiv.style.display = "none"
-}
+//   encodeDiv.style.display = "block";
+//   decodeDiv.style.display = "none"
+// }
 
-function showDecode() {
-  const encodeDiv = document.querySelector('.encodeDiv');
-  const decodeDiv = document.querySelector('.decodeDiv');
+// function showDecode() {
+//   const encodeDiv = document.querySelector('.encodeDiv');
+//   const decodeDiv = document.querySelector('.decodeDiv');
 
-  encodeDiv.style.display = "none";
-  decodeDiv.style.display = "block"
-}
+//   encodeDiv.style.display = "none";
+//   decodeDiv.style.display = "block"
+// }
